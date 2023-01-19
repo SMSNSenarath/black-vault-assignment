011f2799c2d96443a7e7f81297c702ee5b145a10
Shakila Nishadini <smsnshakilaa372@gmail.com>
2023-01-19 12:21:36 +0530

    Add get all posts titles from API

---
+++ src/components/PostList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setPosts(res.data);
+      // console.log(res);
+    });
+  });
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={post.id}>
+            <Link to={post.id}>{post.title}</Link>
+            {/* <h1>{post.title}</h1> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostList;
